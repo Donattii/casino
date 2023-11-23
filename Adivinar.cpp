585a33306bffb3d10bfb35db79a9b15d8ca9a450
Donattii <112676468+Donattii@users.noreply.github.com>
2023-11-23 10:36:19 -0600

    Update Adivinar.cpp COMENTADO

--- Adivinar.cpp
+++ Adivinar.cpp
@@ -1,25 +1,31 @@
 #include "Adivinar.h"
 #include <iostream>
 
+// Constructor de la clase Adivinar
 Adivinar::Adivinar() : Juego("Adivinar"), numeroSecreto(rand() % 5 + 1), intento(0) {}
 
+// Getter para obtener el número de intentos
 int Adivinar::getIntento() const {
     return intento;
 }
 
+// Getter para obtener el número secreto
 int Adivinar::getNumeroSecreto() const {
     return numeroSecreto;
 }
 
+// Función para jugar el juego de adivinar
 bool Adivinar::jugar() {
     std::cin >> intento;
 
+    // Verifica si el intento está en el rango válido [1, 5]
     if (intento >= 1 && intento <= 5) {
+        // Comprueba si el intento es igual al número secreto
         if (intento == numeroSecreto) {
             apuesta = 50;
-            return true;
+            return true;   // Devuelve verdadero si adivina correctamente
         } else {
-            return false;
+            return false;  // Devuelve falso si no adivina correctamente
         }
     } else {
         return false;
