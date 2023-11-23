75f32e80c8e6c747a8e79f3401053f53ef0783de
Donattii <112676468+Donattii@users.noreply.github.com>
2023-11-23 10:37:24 -0600

    Update Dados.h

--- Dados.h
+++ Dados.h
@@ -3,19 +3,25 @@
 
 #include "Juego.h"
 
+// Clase para representar el juego de Dados, hereda de la clase Juego
 class Dados : public Juego {
 private:
-    int dadoJugador;
-    int dadoDealer;
+    int dadoJugador; // Valor del dado del jugador
+    int dadoDealer;  // Valor del dado del dealer
 
 public:
     Dados();
 
+    // Simular el lanzamiento de un dado de 6 caras
     int tirarDado() const;
+
+    // Función para realizar un intento en el juego
     bool jugar();
     
-    // Métodos para obtener los valores de los dados
+    // Obtener el valor del dado del jugador
     int getDadoJugador() const;
+    
+    // Obtener el valor del dado del dealer
     int getDadoDealer() const;
 };
 
