3fb7fc0f212ca0ceae8f2b23f10efe0a0fe6167d
Donattii <112676468+Donattii@users.noreply.github.com>
2023-11-23 09:44:53 -0600

    Update Adivinar.h

--- Adivinar.h
+++ Adivinar.h
@@ -6,10 +6,14 @@
 class Adivinar : public Juego {
 private:
     int numeroSecreto;
+    int intento;
 
 public:
     Adivinar();
     bool jugar() override;
+    
+    // Métodos para obtener el intento del jugador y el número secreto
+    int getIntento() const;
     int getNumeroSecreto() const;
 };
 
