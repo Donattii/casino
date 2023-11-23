f057a53dbff89db53872dc9fe1bd73e6448be519
Donattii <112676468+Donattii@users.noreply.github.com>
2023-11-23 10:40:13 -0600

    Update Jugador.h COMENTADO

--- Jugador.h
+++ Jugador.h
@@ -3,6 +3,7 @@
 
 #include <string>
 
+// Clase para representar un jugador
 class Jugador {
 private:
     std::string nombre;
@@ -10,13 +11,25 @@ private:
     std::string mensajeError;
 
 public:
+    // Constructor de la clase Jugador
     Jugador(const std::string& nombre, int dinero);
+
+    // Obtener el mensaje de error
     std::string getMensajeError() const;
+
+    // Obtener el nombre del jugador
     std::string getNombre() const;
+
+    // Obtener la cantidad de dinero del jugador
     int getDinero() const;
+
+    // Establecer una nueva cantidad de dinero para el jugador
     void setDinero(int nuevoDinero);
 
+    // Apostar una cantidad específica de dinero en un juego
     bool apostar(int cantidad);
+
+    // Jugar un juego específico y gestionar la apuesta
     bool jugar(class Juego& juego);
 };
 
