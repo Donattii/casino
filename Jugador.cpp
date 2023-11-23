434fdaeb9221013a8c7212d8f16c3ba856408e9f
Donattii <112676468+Donattii@users.noreply.github.com>
2023-11-23 10:38:53 -0600

    Update Jugador.cpp COMENTADO

--- Jugador.cpp
+++ Jugador.cpp
@@ -2,20 +2,25 @@
 #include "Juego.h"
 #include <iostream>
 
+// Constructor de la clase Jugador
 Jugador::Jugador(const std::string& nombre, int dinero) : nombre(nombre), dinero(dinero) {}
 
+// Obtener el nombre del jugador
 std::string Jugador::getNombre() const {
     return nombre;
 }
 
+// Obtener la cantidad de dinero del jugador
 int Jugador::getDinero() const {
     return dinero;
 }
 
+// Establecer una nueva cantidad de dinero para el jugador
 void Jugador::setDinero(int nuevoDinero) {
     dinero = nuevoDinero;
 }
 
+// Apostar una cantidad de dinero en un juego
 bool Jugador::apostar(int cantidad) {
     if (cantidad > 0 && cantidad <= dinero) {
         dinero -= cantidad;
@@ -26,10 +31,12 @@ bool Jugador::apostar(int cantidad) {
     }
 }
 
+// Obtener el mensaje de error
 std::string Jugador::getMensajeError() const {
     return mensajeError;
 }
 
+// Jugar un juego específico y gestionar la apuesta
 bool Jugador::jugar(Juego& juego) {
     if (apostar(10)) {
         juego.jugar();
