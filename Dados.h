7205e4feb769e8462f58733d9d1651d61f5e11bd
Donattii <112676468+Donattii@users.noreply.github.com>
2023-11-23 09:42:57 -0600

    Update Dados.h

--- Dados.h
+++ Dados.h
@@ -4,11 +4,19 @@
 #include "Juego.h"
 
 class Dados : public Juego {
+private:
+    int dadoJugador;
+    int dadoDealer;
+
 public:
     Dados();
 
     int tirarDado() const;
     bool jugar();
+    
+    // Métodos para obtener los valores de los dados
+    int getDadoJugador() const;
+    int getDadoDealer() const;
 };
 
 #endif
