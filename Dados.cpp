024da555db549dce53a05d3ca5f53767caace9c2
Donattii <112676468+Donattii@users.noreply.github.com>
2023-11-23 09:42:35 -0600

    Update Dados.cpp

--- Dados.cpp
+++ Dados.cpp
@@ -1,14 +1,22 @@
 #include "Dados.h"
 
-Dados::Dados() : Juego("Dados") {}
+Dados::Dados() : Juego("Dados"), dadoJugador(0), dadoDealer(0) {}
+
+int Dados::getDadoJugador() const {
+    return dadoJugador;
+}
+
+int Dados::getDadoDealer() const {
+    return dadoDealer;
+}
 
 int Dados::tirarDado() const {
     return rand() % 6 + 1;
 }
 
 bool Dados::jugar() {
-    int dadoJugador = tirarDado();
-    int dadoDealer = tirarDado();
+    dadoJugador = tirarDado();
+    dadoDealer = tirarDado();
 
     if (dadoJugador > dadoDealer) {
         apuesta = 20;
