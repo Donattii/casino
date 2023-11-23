9a47f74d8c5a0e741dfa715fc46dd75c47ec7e4f
Donattii <112676468+Donattii@users.noreply.github.com>
2023-11-23 10:40:37 -0600

    Update PPT.cpp COMENTADO

--- PPT.cpp
+++ PPT.cpp
@@ -1,30 +1,35 @@
 #include "PPT.h"
 #include <iostream>
 
+// Constructor de la clase PPT
 PPT::PPT() : Juego("Piedra, Papel o Tijera"), eleccionJugador(0), eleccionDealer(0) {}
 
+// Obtener la elección del jugador
 int PPT::getEleccionJugador() const {
     return eleccionJugador;
 }
 
+// Obtener la elección del dealer
 int PPT::getEleccionDealer() const {
     return eleccionDealer;
 }
 
+// Iniciar el juego de Piedra, Papel o Tijera
 bool PPT::jugar() {
     std::cin >> eleccionJugador;
 
     eleccionDealer = rand() % 3 + 1;
 
+    // Determinar el resultado del juego y establecer la apuesta
     if ((eleccionJugador == 1 && eleccionDealer == 3) ||
         (eleccionJugador == 2 && eleccionDealer == 1) ||
         (eleccionJugador == 3 && eleccionDealer == 2)) {
         apuesta = 20;
-        return true;
+        return true;   // Devolver verdadero si el jugador gana
     } else if (eleccionJugador == eleccionDealer) {
         apuesta = 10;
-        return false;
+        return false;  // Devolver falso si hay un empate
     } else {
-        return false;
+        return false;  // Devolver falso si el jugador pierde
     }
 }
