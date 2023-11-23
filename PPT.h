a48ecd43a6aa8fa450fb3d75c39757a937f45c52
Donattii <112676468+Donattii@users.noreply.github.com>
2023-11-23 10:40:59 -0600

    Update PPT.h COMENTADO

--- PPT.h
+++ PPT.h
@@ -3,6 +3,7 @@
 
 #include "Juego.h"
 
+// Clase para representar el juego de Piedra, Papel o Tijera, hereda de la clase Juego
 class PPT : public Juego {
 private:
     int eleccionJugador;
@@ -10,9 +11,14 @@ private:
 
 public:
     PPT();
+
+    // Función para realizar un intento en el juego
     bool jugar() override;
-    
+
+    // Obtener la elección del jugador
     int getEleccionJugador() const;
+
+    // Obtener la elección del dealer
     int getEleccionDealer() const;
 };
 
