e734e89c1090210d14bdf935fb739b77f5a0f5eb
Donattii <112676468+Donattii@users.noreply.github.com>
2023-11-23 09:43:48 -0600

    Update PPT.h

--- PPT.h
+++ PPT.h
@@ -4,9 +4,16 @@
 #include "Juego.h"
 
 class PPT : public Juego {
+private:
+    int eleccionJugador;
+    int eleccionDealer;
+
 public:
     PPT();
     bool jugar() override;
+    
+    int getEleccionJugador() const;
+    int getEleccionDealer() const;
 };
 
 #endif
