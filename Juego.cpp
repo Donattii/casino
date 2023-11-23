e4e2b4db5136a4e6d8a6845c6c65177de1460b8a
Donattii <112676468+Donattii@users.noreply.github.com>
2023-11-23 10:37:43 -0600

    Update Juego.cpp COMENTADO

--- Juego.cpp
+++ Juego.cpp
@@ -1,15 +1,19 @@
 #include "Juego.h"
 
+// Constructor de la clase Juego
 Juego::Juego(const std::string& nombre) : nombre(nombre), apuesta(0) {}
 
+// Obtener el nombre del juego
 const std::string& Juego::getNombre() const {
     return nombre;
 }
 
+// Obtener la apuesta actual del juego
 int Juego::getApuesta() const {
     return apuesta;
 }
 
+// Establecer una nueva apuesta para el juego
 void Juego::setApuesta(int nuevaApuesta) {
     apuesta = nuevaApuesta;
 }
