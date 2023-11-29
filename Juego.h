e8d185f29368387a7601769931c0648b7f351ff2
Donattii <112676468+Donattii@users.noreply.github.com>
2023-11-28 23:00:59 -0600

    Update Juego.h

--- Juego.h
+++ Juego.h
@@ -3,27 +3,25 @@
 
 #include <string>
 
+using namespace std;
+
 // Clase base para representar un juego
 class Juego {
 protected:
-    std::string nombre;
+    string nombre;
     int apuesta;
 
 public:
-    // Constructor de la clase Juego
-    Juego(const std::string& nombre);
+    Juego(const string& nombre);
 
-    // Función virtual pura para jugar el juego (debe ser implementada por las clases derivadas)
+    // Función virtual para jugar el juego (debe ser implementada por las clases derivadas)
     virtual bool jugar() = 0;
 
     // Obtener la apuesta actual del juego
     int getApuesta() const;
     
     // Obtener el nombre del juego
-    const std::string& getNombre() const;
-
-    // Establecer una nueva apuesta para el juego
-    void setApuesta(int nuevaApuesta);
+    const string& getNombre() const;
 };
 
 #endif
